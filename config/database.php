4e24fd07a552b1ed051afd25d86e008e97206e4e
nx-exadm <exadm@norielix.com>
2026-08-03 09:10:21 +0530

    Change default database connection to MySQL

--- config/database.php
+++ config/database.php
@@ -1,15 +1,26 @@
 <?php
 
 return [
-    'default' => env('DB_CONNECTION', 'libsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     'connections' => [
-        'libsql' => [
-            'driver' => 'libsql',
-            'database' => '',
-            'url' => env('DB_URL', ''),
-            'authToken' => env('DB_AUTH_TOKEN', ''),
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
     ],
 
