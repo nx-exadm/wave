7aaa97a4f62355e8ce9006c743256c3973aa5ec4
nx-exadm <exadm@norielix.com>
2026-08-03 11:54:36 +0530

    Remove LibSQLDriverServiceProvider from providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,5 +5,4 @@ return [
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\FolioServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
-    Turso\Driver\Laravel\LibSQLDriverServiceProvider::class,
 ];
